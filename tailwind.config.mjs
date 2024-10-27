87e4a99a2f0c61623fa9e1792f53c328c88ec2a8
Camilla <caru0001@stud.kea.dk>
2024-10-27 19:35:31 +0100

    scrollbar, farver og baggrund

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-ocean": "#004153",
+        "custom-orange": "#F19A48",
+        "custom-sand": "#F3EADC",
+        "custom-lightblue": "#BFDEE3",
+        "custom-offwhite": "#EEEEEE",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
